52ed5176a718bd8aec1633b34612d7b9f0d7891d
suyash_parve <suyashparve113sp@gmail.com>
2025-09-27 11:26:04 +0530

    complete loops_array exercises

--- loops_array_ex.js
+++ loops_array_ex.js
@@ -25,3 +25,47 @@ function filterRangeInPlace(arr, a, b) {
 let arr1 = [5, 3, 8, 1]
 console.log(filterRangeInPlace(arr1,1,4))
 console.log(arr1)
+// -----------------------------------------------
+function sortDecrese(arr2) {
+    arr2.sort((a, b) => b - a)
+    return arr2
+}
+arr2 = [5, 2, 1, -10, 8]
+console.log(sortDecrese(arr2))
+// --------------------------------
+
+// function copySorted(arr3) {
+//     let sorted = arr3
+//     sorted.slice().sort();
+//     return sorted
+// }
+// let arr3 = ["HTML", "JavaScript", "CSS"];
+// console.log(copySorted(arr3))
+
+function copySorted(arr) {
+  return arr.slice().sort();
+}
+
+let arr3 = ["HTML", "JavaScript", "CSS"];
+
+let sorted = copySorted(arr3);
+
+console.log(sorted)
+console.log(arr3)
+// -----------------------------------------
+
+function unique(arr) {
+    let unique = []
+    for (let str of arr) {
+        if (!unique.includes(str)) {
+            unique.push(str)
+        }
+    }
+    return unique
+  }
+  
+  let strings = ["Hare", "Krishna", "Hare", "Krishna",
+    "Krishna", "Krishna", "Hare", "Hare", ":-O"
+  ];
+
+  console.log(unique(strings))
